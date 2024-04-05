77403014be4b5851d7ab53d18382d50131f8754a
CYC <141478442+CharlotteYichia@users.noreply.github.com>
2024-04-06 03:44:27 +0900

    Delete week1 directory

--- week1/app.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  let phonemenu = document.querySelector("#rwd-phone-menu");
-
-  if (phonemenu.style.display === "block") {
-    phonemenu.style.display = "none";
-  } else {
-    phonemenu.style.display = "block";
-  }
-}
