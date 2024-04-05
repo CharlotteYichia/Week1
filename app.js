fbdcebbcf547bc7995935bea38366a2ec8c33d94
CYC <141478442+CharlotteYichia@users.noreply.github.com>
2024-04-06 03:44:58 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  let phonemenu = document.querySelector("#rwd-phone-menu");
+
+  if (phonemenu.style.display === "block") {
+    phonemenu.style.display = "none";
+  } else {
+    phonemenu.style.display = "block";
+  }
+}
